7b22e731449b937265e1928e4c44bdac932ac65b
Alexey <razgonov.aleksej@yandex.ru>
2025-10-10 21:27:30 +0700

    setting up auth

---
+++ auth-service/app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    
+}
