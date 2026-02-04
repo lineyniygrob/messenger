fc55a92fe79a9a37ba17314a03eaebf666d3431e
Alexey <razgonov.aleksej@yandex.ru>
2026-02-04 22:41:36 +0700

    небольшие изменения в контроллере

--- auth-service/app/Http/Controllers/AuthController.php
+++ auth-service/app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Services\LoginService;
 use App\Services\RegisterService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -19,18 +20,18 @@ class AuthController extends Controller
         private LoginService $loginService
     ){}   
 
-    public function register(RegisterRequest $request)
+    public function register(RegisterRequest $request): JsonResponse
     {
         $validate = $request->validated();
         $token = $this->registerService->register($validate);
 
         return response()->json([
             "token" => $token->plainTextToken,
-        ], 201);
+        ], Response::HTTP_CREATED);
 
     }
 
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         $credentials = $request->validated();
         $token = $this->loginService->login($credentials);
@@ -38,17 +39,17 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'авторизация успешна',
             'token' => $token,
-        ], 200);
+        ], Response::HTTP_OK);
     }
 
-    public function logout(Request $request)
+    public function logout(Request $request): JsonResponse
     {
         $user = $request->user();
         $user->currentAccessToken()->delete();
 
         return response()->json([
             'message' => 'Вы вышли из аккаунта',
-        ], 200);
+        ], Response::HTTP_OK);
     }
 
     public function test()
